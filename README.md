87a54769f98bf861a18da9d53c925036526fe193
Trevor F. Smith <trevor@trevor.smith.name>
2011-08-19 15:14:40 -0700

    Stubbed out a Collection class and added a simple build.xml

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # libSBOLcore: SBOL as represented by Java objects
 
+We are in the process of moving around the core classes.  They will eventually end up here.
---
+++ src/org/sbolstandard/core/Collection.java
@@ -0,0 +1,14 @@
+package org.sbolstandard.core;
+
+public class Collection {
+	/**
+	* Elements that are intended as engineering components in synthetic biology.
+	*
+	* For example, standard biological parts, BioBricks, oligo components,
+	* vector plasmids, genomes, or any other DNA segment of interest as a building
+	* block of biological systems.
+	*
+	*/
+	public Collection() { }
+
+}
